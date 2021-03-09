264ee33f791390b24dec9545927f51e1a1908172
Chris Rogers <47615352+chrislrogers@users.noreply.github.com>
2021-03-09 20:32:03 +0000

    formatting changes

--- numberguesser.c
+++ numberguesser.c
@@ -4,40 +4,40 @@
 
 int main(int argc, char *argv[])
 {
-    int number=1;
-    int input=0;
-    int score=0;
+    int number = 1;
+    int input = 0;
+    int score = 0;
 
-    if(argc==1) 
+    if (argc == 1)
     {
-        number=100;
+        number = 100;
     }
-    if(argc==2) 
+    if (argc == 2)
     {
-        number=atoi(argv[1]);
+        number = atoi(argv[1]);
     }
-    if(argc>=3) 
+    if (argc >= 3)
     {
         exit(1);
     }
 
     srand(time(0));
-    int target = ((rand() % number ) + 1);
+    int target = ((rand() % number) + 1);
 
-        do 
+    do
+    {
+        printf("Enter a number between 1 and %d (inclusive)\n: ", number);
+        scanf("%d", &input);
+        score++;
+        if (input < target)
         {
-            printf("Enter a number between 1 and %d (inclusive)\n: ", number);
-            scanf("%d", &input);  
-            score++;
-            if (input < target)
-            {
-                printf("You need to guess higher\n");
-            }
-            else if (input > target)
-            {
-                printf("You need to guess lower\n");
-            }
-        } while (input != target);
+            printf("You need to guess higher\n");
+        }
+        else if (input > target)
+        {
+            printf("You need to guess lower\n");
+        }
+    } while (input != target);
 
     printf("Correct the answer is: %d, you took %d attempt(s)\n", target, score);
 
